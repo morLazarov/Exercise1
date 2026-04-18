5f9c7966e0ae3ef9d9fbe69d1379688221ae509e
Mor Lazarov <mor.lazarov@gmail.com>
2026-04-18 17:48:30 +0300

    WIP

--- src/insurance.py
+++ src/insurance.py
@@ -13,26 +13,41 @@ REGION_COLUMN = "region"
 
 TEST_SIZE = 0.2
 
-TIKHONOV_REG_PARAMETER = [0.1, 0.01, 0.001]
+TIKHONOV_REG_PARAMETER = 0.01
+
+EXPERIMENT_NUMBER = "experiment_number"
+DATASET_VERSION = "dataset_version"
+MODEL_TYPE = "model_type"
+TRAIN_MSE = "train_mse"
+TEST_MSE = "test_mse"
+RATIO_TO_SCALAR_MODEL = "ratio_to_scalar_model"
+
+SCALAR_MODEL_TYPE = "scalar_model"
+LS_MODEL_TYPE = "ls_model"
+
+LS_MODEL_TRAINING_ITERATIONS = 10
 
 path = '../input/'
 df = pd.read_csv(path+'insurData.csv')
 print('\nNumber of rows and columns in the data set: ', df.shape)
 print('')
 
+results_df = pd.DataFrame({EXPERIMENT_NUMBER: [], DATASET_VERSION: [], MODEL_TYPE: [], TRAIN_MSE: [], TEST_MSE: [], RATIO_TO_SCALAR_MODEL: []})
+
 
 """
     Returns a split variation of the dataset
     usage:
-    X_train, X_test, y_train, y_test = split_df(df, test_size)
+    X, y, X_train, X_test, y_train, y_test = split_df(df, test_size)
 """
-def split_df(_df: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def split_df(_df: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     X = _df.drop(columns=[TARGET_COLUMN])
     y = _df[TARGET_COLUMN]
-    return train_test_split(X, y, test_size=test_size)
+    return X, y, *train_test_split(X, y, test_size=test_size)
 
 
-def preprocess(_df: pd.DataFrame):
+def preprocess1(_base_df: pd.DataFrame):
+    _df = _base_df.copy()
     _df[TARGET_COLUMN] = _df[TARGET_COLUMN]/1000 #divide charges by 1000
     _df[SEX_COLUMN] = _df[SEX_COLUMN].apply(lambda x: 1 if x=="male" else 0)
     _df[SMOKER_COLUMN] = _df[SMOKER_COLUMN].apply(lambda x: 1 if x=="yes" else 0)
@@ -41,6 +56,13 @@ def preprocess(_df: pd.DataFrame):
 
     return _df
 
+def preprocess2(_base_df: pd.DataFrame):
+    _df = _base_df.copy()
+    _df[TARGET_COLUMN] = _df[TARGET_COLUMN]/1000 #divide charges by 1000
+    _df[SEX_COLUMN] = _df[SEX_COLUMN].apply(lambda x: 1 if x=="male" else 0)
+    _df = _df.assign(a0 = 1) #Add a column of 1's to a copy of the dataset
+
+    return _df
 
 #Solves LS for a given matrix, b vector and alpha parameter and returns the minimizing vector
 def tikhonov_regularized_solve(A: np.ndarray, b: np.ndarray, _reg_param: float) -> np.ndarray: 
@@ -88,40 +110,71 @@ def sanity_check(column_names, y_test, y_pred, _alpha_arr):
     print(f"alpha_arr: {_alpha_arr}")
     plot_pred_vs_gt(y_test, y_pred)
 
-print(df.head())
+def calculate_MSE(gt, pred):
+    return np.square(np.subtract(gt, pred)).mean()
 
-df = preprocess(df)
-columns_names = list(df.columns)
-X_train, X_test, y_train, y_test = split_df(df, TEST_SIZE)
+def record_experiment(experiment_number, dataset_version, model_type, train_mse, test_mse, ratio_to_scalar_model):
+    results_df.loc[len(results_df)] = [experiment_number, dataset_version, model_type, train_mse, test_mse, ratio_to_scalar_model]
 
-#Lets look into top few rows and columns in the dataset
-print(X_train.head())
-print(y_train.head())
-print(X_test.head())
-print(y_test.head())
+def evaluation_pipeline(_df: pd.DataFrame, dataset_version: str):
+    X, y, X_train, X_test, y_train_gt, y_test_gt = split_df(_df, TEST_SIZE)
 
-print(X_train.shape)
-print(y_train.shape)
-train_samples = y_train.shape
-print(X_test.shape)
-print(y_test.shape)
-test_samples = y_test.shape
+    samples_count = len(y)
 
-X_train = X_train.to_numpy()
-X_test = X_test.to_numpy()
-y_train = y_train.to_numpy()
-y_test = y_test.to_numpy()
-
-print(X_train)
-
-#### Single scalar model ####
+    X = X.to_numpy()
+    y = y.to_numpy()
+    X_train = X_train.to_numpy()
+    X_test = X_test.to_numpy()
+    y_train_gt = y_train_gt.to_numpy()
+    y_test_gt = y_test_gt.to_numpy()
 
 
-#### Least squares model ####
-for reg_param in TIKHONOV_REG_PARAMETER:
-    _alpha_arr = tikhonov_regularized_solve(X_train, y_train, reg_param)
-    y_pred = np.matmul(X_test, _alpha_arr)
-    
-    print(f"regularization parameter: {reg_param}")
-    print(f"MSE: {((np.linalg.norm(y_test-y_pred))**2)/test_samples}")
-    sanity_check(column_names=columns_names, y_test=y_test, y_pred=y_pred, _alpha_arr=_alpha_arr)
+    # Plot histogram
+    plt.figure()
+    plt.hist(y, bins=30, density=True)
+    plt.xlabel("Value")
+    plt.ylabel("Density")
+    plt.title("Distribution of values")
+    plt.show()
+
+    print(X_train)
+
+
+    ### Single Scalar Model ###
+    min_arg = (1/samples_count) * np.sum(y)
+    a_0 = np.full(samples_count, min_arg)
+    mse_0 = calculate_MSE(y, a_0)
+    print(f"Scalar model MSE: {mse_0}")
+
+    record_experiment(1, dataset_version, SCALAR_MODEL_TYPE, mse_0, mse_0, 1)
+
+    for i in range(1, LS_MODEL_TRAINING_ITERATIONS+1):
+        #### Least squares model ####
+        print(f"regularization parameter: {TIKHONOV_REG_PARAMETER}")
+
+        _alpha_arr = tikhonov_regularized_solve(X_train, y_train_gt, TIKHONOV_REG_PARAMETER)
+
+        y_train_pred = np.matmul(X_train, _alpha_arr) #final predicted y values
+        train_mse = calculate_MSE(y_train_gt, y_train_pred)
+        print(f"train MSE: {train_mse}")
+
+        y_test_pred = np.matmul(X_test, _alpha_arr) #final predicted y values
+        test_mse = calculate_MSE(y_test_gt, y_test_pred)
+        print(f"test MSE: {test_mse}")
+
+        print(f"learned weights: {_alpha_arr}")
+
+        record_experiment(i, dataset_version, LS_MODEL_TYPE, train_mse, test_mse, test_mse / mse_0)
+
+
+preprocessed_df = preprocess1(df)
+print(preprocessed_df.columns)
+print("#### evaluation with smoking and regions")
+evaluation_pipeline(preprocessed_df, "with_smoker_and_region")
+
+preprocessed_df2 = preprocess2(df.drop(columns=[SMOKER_COLUMN, REGION_COLUMN]))
+print(preprocessed_df2.columns)
+print("#### evaluation without smoking and regions")
+evaluation_pipeline(preprocessed_df2, "no_smoker_and_region")
+
+print(results_df)
